90c0f2de3b8a18be37179f68af8a3d4b30e3a5aa
DINI513 <irisha1555@gmail.com>
2019-02-26 22:10:01 +0500

    Добавьте файлы проекта.

---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,59 @@
+﻿// lab_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <pch.h>
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+const int massCount = 1000001;
+const int threadCount = 4;
+
+double massMax;
+std::mutex locker;
+
+void Calculator(double *mass, int numThread, double *result)
+{
+	printf_s("Thread #%i started\n", numThread);
+	int column = numThread - 1;
+	result[column] = 0;
+	for (int i = column; i < massCount; i += threadCount)
+	{
+		result[column] += mass[i];
+		locker.lock();
+		if (massMax < mass[i])
+		{
+			massMax = mass[i];
+		}
+		locker.unlock();
+	}
+	printf_s("Thread #%i stopped\n", numThread);
+}
+
+int main()
+{
+	double *mass = new double[massCount];
+	double *massSumm = new double[threadCount];
+	for (int i = 0; i < massCount; ++i)
+	{
+		mass[i] = (rand() % 100 + 1) / (float)(rand() % 100 + 1);
+	}
+	std::thread *massThreads = new std::thread[threadCount];
+	for (int i = 0; i < threadCount; i++)
+	{
+		massThreads[i] = std::thread(Calculator, mass, i + 1, massSumm);
+	}
+	for (int i = 0; i < threadCount; i++)
+	{
+		massThreads[i].join();
+	}
+	double summ = 0;
+	for (int i = 0; i < threadCount; i++)
+	{
+		summ += massSumm[i];
+	}
+	printf_s("Summ %f \n", summ);
+	printf_s("Max %f \n", massMax);
+	system("pause");
+	return 0;
+}
